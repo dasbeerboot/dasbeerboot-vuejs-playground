9f45ce3d07d85fe52715b8ba02d4aab883a09de9
Juwon Chun <juwonchun@gmail.com>
2021-09-27 13:57:52 +0900

    WIP: modal not working

---
+++ src/components/modal/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <transition name="modal-animation">
+    <div v-show="isModalOpen" class="test-modal">
+      <transition name="modal-animation-inner">
+        <div class="test-modal-inner">
+          <div v-show="isModalOpen">
+            opened!
+            <slot />
+            <my-button text="close" />
+          </div>
+        </div>
+      </transition>
+    </div>
+  </transition>
+</template>
+
+<script>
+import MyButton from '../button/MyButton.vue'
+
+export default {
+  components: { MyButton },
+  props: [{ isModalOpen: { type: Boolean } }],
+  setup() {},
+  methods: {
+    closeModal() {
+      this.isModalOpen = false
+    },
+  },
+}
+</script>
+
+<style></style>
--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
+  <div class="hello-world-wrapper">
     <h1>"Hello World"</h1>
     <router-link to="/">
-      <my-button text="prop test" />
+      <my-button text="prop test(go back)" />
     </router-link>
+    <!-- <my-button text="open modal" @click="toggleIsModalOpen" />
+    <modal :isModalOpen="isModalOpen">
+      <div class="modal-content">
+        <h1>my test modal</h1>
+      </div>
+    </modal>  -->
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+// import Modal from '../components/modal/Modal.vue'
 import MyButton from '../components/button/MyButton.vue'
+
 export default {
   components: { MyButton },
+  // components: { MyButton, Modal },
+  setup() {
+    const isModalOpen = ref(true)
+    return { isModalOpen }
+  },
+  methods: {
+    toggleIsModalOpen() {
+      this.isModalOpen = false
+    },
+  },
 }
 </script>
+
+<style>
+.hello-world-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
